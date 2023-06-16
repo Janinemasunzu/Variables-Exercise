b1c0cbb32184388e54713db71573ff450f7b8afd
Janine Masunzu <jamasunzu99@gmail.com>
2023-06-16 15:14:04 -0500

    variable exercise done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //starting writing your code here
+
+            string name = "Janine";
+            int years = 23;
+            char secondInitial = 'M';
+            bool isOver18 = true;
+            decimal bill = 79.99m;
+
+            Console.WriteLine($"My name is {name}, I am {years} years old , My Second initial is {secondInitial}, I am Over 18 years {isOver18}, my phone bill is {bill} ");
+            
+          
+            
+            
+           
         }
     }
 }
